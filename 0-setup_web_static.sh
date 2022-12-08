6f324b516b2e368d37d52003dc2fb9d5f3b6173d
Favour <favourkolawole21@gmail.com>
2022-12-08 22:06:03 +0100

    Airbnb clone V2

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Prepare your web servers
+apt -y update
+apt install -y nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
+sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+
+service nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Fabric script'''
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    '''do pack module'''
+
+    file_name = "web_static_" + \
+        datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+    save_dir = "versions/"
+
+    local("mkdir -p " + save_dir)
+    chk = local("tar -cvzf {}{} web_static".format(save_dir, file_name))
+    if chk.succeeded:
+        return (save_dir + file_name)
+    else:
+        return (None)
